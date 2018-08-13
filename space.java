47bde5b2c36dff20200c154017399d4a11b152a0
lakshmiobulasetti <41731019+lakshmiobulasetti@users.noreply.github.com>
2018-08-13 10:45:53 +0530

    Update and rename README.md to space.java

---
+++ space.java
@@ -0,0 +1,41 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+            String test = "luckyraj";
+		count(test);
+
+	}
+	public static void count(String x){
+		char[] ch = x.toCharArray();
+		int letter = 0;
+		int space = 0;
+		int num = 0;
+		int other = 0;
+		for(int i = 0; i < x.length(); i++){
+			if(Character.isLetter(ch[i])){
+				letter ++ ;
+			}
+			else if(Character.isDigit(ch[i])){
+				num ++ ;
+			}
+			else if(Character.isSpaceChar(ch[i])){
+				space ++ ;
+			}
+			else{
+				other ++;
+			}
+		}
+		System.out.println("luckyraj");
+		System.out.println("letter: " + letter);
+		System.out.println("space: " + space);
+		System.out.println("number: " + num);
+		System.out.println("other: " + other);
+			}
+}
+
